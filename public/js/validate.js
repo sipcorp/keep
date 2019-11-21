a02d5fc2556d8e2cbcba64eff9ca69a0bf8f9a24
porfirio Castillo <sipcorpinfo@gmail.com>
2019-11-21 15:31:33 -0500

    Commit message

--- public/js/validate.js
+++ public/js/validate.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
 $('.price-list').popover();
-
+	
+	// flop
+	$('.fliper').click(function(){
+		var id = $(this).parentsUntil()[4].id
+		$('.image-flip[id='+id+']').toggleClass('hover');
+	});
 });
