3217ff491c75cf741e470f507d7e7a4b97dffae9
porfirio Castillo <sipcorpinfo@gmail.com>
2019-11-22 09:36:20 -0500

    Commit message

--- public/js/validate.js
+++ public/js/validate.js
@@ -6,4 +6,26 @@ $('.price-list').popover();
 		var id = $(this).parentsUntil()[4].id
 		$('.image-flip[id='+id+']').toggleClass('hover');
 	});
+
+	$("input[type=radio]").on("click",function(){
+		var id = this.id
+		if(id == "first_toggle"){
+			$("section#virus").removeClass('out').removeClass('active')
+			$("section#bacterias").removeClass('out').addClass('active');
+			$("section#hongos").removeClass('out').removeClass('active')
+		}
+		if(id == "second_toggle"){
+			$("section#bacterias").removeClass("active").addClass('out');
+			$("section#hongos").removeClass('active').addClass('out');
+			$("section#virus").removeClass("out").addClass('active');
+		}
+		if(id == "third_toggle"){
+			$("section#bacterias").removeClass("active").addClass('out');
+			$("section#virus").removeClass('active').addClass('out');
+			$("section#hongos").removeClass("out").addClass('active');
+		}
+	})
+	function HideTheElementAfterAnimation(id){
+		$("section#"+id).css("display", "none");
+	}
 });
