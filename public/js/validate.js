ba8a8a8c97a9803f3396f2bd00bf89e2183f8e36
porfirio Castillo <sipcorpinfo@gmail.com>
2019-11-22 11:55:40 -0500

    Commit message

--- public/js/validate.js
+++ public/js/validate.js
@@ -28,4 +28,17 @@ $('.price-list').popover();
 	function HideTheElementAfterAnimation(id){
 		$("section#"+id).css("display", "none");
 	}
+	mybutton = document.getElementById("myBtn");
+	
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+	
+	function scrollFunction() {
+	  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	  } else {
+		mybutton.style.display = "none";
+	  }
+	}
+	
 });
